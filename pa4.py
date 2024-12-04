f11dd5e384ba1c07d0ef7a54d5c6c507d2ba483d
imyourCxxmp <Chanawee19848@gmail.com>
2024-12-04 09:48:59 +0700

    Add files via upload

--- pa4.py
+++ pa4.py
@@ -1,36 +1,31 @@
 import streamlit as st
 import pandas as pd
+import json
 import openai
 
-# Sidebar for API key
-st.sidebar.title("API Key")
-user_api_key = st.sidebar.text_input("Enter your OpenAI API Key:", type="password")
+user_api_key = st.sidebar.text_input("OpenAI API key")
 client = openai.OpenAI(api_key=user_api_key)
-# Input options
-st.title("Smart Passage Analyzer")
-input_text = st.text_area("Enter your passage here:")
-prompt = (
-                f"Analyze the following text: {input_text}\n"
-                "1. Create 3 Cloze Test questions with answers and explanations.\n"
-                "2. Extract vocabulary with part of speech, translations, and difficulty levels."
-            )
+prompt = """Act as an english exam writer who want undergraduate student to develop their reading comprehension skills.
+1.Your task is generating reading comprehension exam from keyword provided by user.
+2.You have to fact check and prove before using the information.
+3.The article should be 400-500 words , vocabulary should be B2-C1 following the CEFR level and grammar must correct and no error.
+4.The exam should be 10 questions with 4 multiple choice that complex and challenging that require deeper comprehension and interpretation.
+5.Then, you have to provide the answer key description with proper reasons for every questions in the table.
+"""
 
-if st.button("Analyze"):
-        # Process input and call OpenAI Completion API
-        if input_text:
-            messages_so_far = [
+st.title('Reading exam generator')
+st.markdown('AI ช่วยแต่งข้อสอบพาร์ต Reading เพียงแค่เขียน keyword ที่จะต้องการ \n Article จะมีความยาว 400-500 คำ ความยากระดับ B1-B2 และมีข้อสอบทั้งหมด 10 ข้อ')
+user_input = st.text_area('text your keyword here')
+
+if st.button('Click'):
+    messages_so_far = [
         {"role": "system", "content": prompt},
-        {'role': 'user', 'content': input_text},
-            ]
-            try:
-                response = client.chat.completions.create(
-                    model="gpt-4o-mini", 
-                    messages=messages_so_far,
-                    temperature=1.1
-                )
-                result = response.choices[0].text.strip()
-                st.write("**Analysis Result:**")
-                st.text(result)
-
-            except Exception as e:
-                st.error(f"Error occurred: {e}")
+        {'role': 'user', 'content': user_input},
+    ]
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=messages_so_far
+    )
+    st.markdown('**AI response:**')
+    answer = response.choices[0].message.content
+    print(answer)
